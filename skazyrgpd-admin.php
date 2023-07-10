6a51bb34a1addc6139a97c188d66c82bc508f5c2
lucas.desfontaine <lucas.desfontaine1@gmail.com>
2023-07-10 11:18:14 +1100

    fix(showData): afficher la data de la db dans le form

--- skazyrgpd-admin.php
+++ skazyrgpd-admin.php
@@ -11,35 +11,46 @@
         include 'skazyrgpd-settings.php';
         $skazyrgpd_db = $wpdb->prefix . "skazyrgpd";
         $results = $wpdb->get_results("SELECT setting_name, setting_description, setting_value, setting_type, setting_select_possible_values FROM $skazyrgpd_db", ARRAY_N);
+        
+
         foreach ($results as $result) {
-            $settingName = $result[0];
-            $settingDesc = $result[1];
-            $settingVal = $result[2];
-            $settingType = $result[3];
-            $settingSelect = $result[4];
-            //var_dump($settingSelect);
-            echo "<div>";
-            if ($result[3] == "select") {
-                echo "$settingDesc : <br><select name='$settingName'>";
-                /*foreach ($settingSelect as $option) {
-                    var_dump($option);
-                    die();
-                    echo "<option value='$option'>$option</option>";
-                }*/
-                echo "</select>";
-            } else {
-                echo "$settingDesc : <br><input type='$settingType' name='$settingName'>";
+            $setting_name = $result[0];
+            $setting_description = $result[1];
+            $setting_value = $result[2];
+            $setting_type = $result[3];
+            $setting_select_possible_values = $result[4];
+            if ($setting_type == "text") {
+                echo "<label for='$setting_name'>$setting_description</label><br>";
+                echo "<input type='text' name='$setting_name' value='$setting_value'><br>";
+                }
+            elseif ($setting_type == "color") {
+                echo "<label for='$setting_name'>$setting_description</label><br>";
+                echo "<input type='color' name='$setting_name' value='$setting_value'><br>";
+
+            } else if ($setting_type == "select") {
+                echo "<label for='$setting_name'>$setting_description</label><br>";
+                echo "<select name='$setting_name'>";
+                $setting_select_possible_values = explode(",", $setting_select_possible_values);
+                $setting_select_possible_values = str_replace("'", "", $setting_select_possible_values);
+                $setting_select_possible_values = str_replace("[", "", $setting_select_possible_values);
+                $setting_select_possible_values = str_replace("]", "", $setting_select_possible_values);
+                foreach ($setting_select_possible_values as $setting_select_possible_value) {
+                    if ($setting_select_possible_value == $setting_value) {
+                        echo "<option value='$setting_select_possible_value' selected>$setting_select_possible_value</option>";
+                    } else {
+                        echo "<option value='$setting_select_possible_value'>$setting_select_possible_value</option>";
+                    }
+                }
+                echo "</select><br>";
+            } else if ($setting_type == "textarea") {
+                echo "<label for='$setting_name'>$setting_description</label><br>";
+                echo "<textarea name='$setting_name'>$setting_value</textarea><br>";
+            } else if ($setting_type == "checkbox") {
+                echo "<label for='$setting_name'>$setting_description</label><br>";
+                if ($setting_value == "true") {
+                    echo "<input type='checkbox' name='$setting_name' value='true' checked><br>";
+                } else {
+                    echo "<input type='checkbox' name='$setting_name' value='true'><br>";
+                }
             }
-            echo "</div><br>";
         }
-        echo $query; //affiche la requête SQL pour créer la base de données
-        ?>
-    </form>
-    <ul>
-        <?php
-        foreach ($SettingsTo as $desc => $setting) {
-            echo "<li>" . $desc . " : " . $setting . "</li>";
-        }
-        ?>
-    </ul>
-</div>
